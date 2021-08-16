77b9fb586e179c509113675032219ad7359bc6cc
Thomas.Cornes <Thomas.cornes@my.jcu.edu.au>
2021-08-16 20:52:34 +1000

    get Passcode function

--- Prac_03/Password_Check.py
+++ Prac_03/Password_Check.py
@@ -11,3 +11,14 @@ def initial_version():
         password = input("Enter password of at least {} characters: ".format(MIN_LENGTH))
 
     print('*' * len(password))
+
+
+# Initial_version()
+
+def get_passcode():
+    passcode = input("Enter passcode of at least {} Characters").format(MIN_LENGTH)
+    while passcode < MIN_LENGTH:
+        print("Password too Short")
+        passcode = input("Enter passcode of at least {} Characters").format(MIN_LENGTH)
+    return passcode
+
