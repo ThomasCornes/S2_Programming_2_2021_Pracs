47ae30233c68f0371ec7a3bb19e9169952c50b2c
Thomas.Cornes <Thomas.cornes@my.jcu.edu.au>
2021-08-23 16:28:49 +1000

    List_Warmup.py finished

---
+++ Prac_04/Lists_Warmup.py
@@ -0,0 +1,21 @@
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# print(numbers[0])
+
+# 1. Change first element to ten
+numbers = ["ten", 1, 4, 1, 5, 9, 2]
+print(numbers[0])
+
+#2. Change the last element of numbers to 1
+numbers = [3, 1, 4, 1, 5, 9, 1]
+print(numbers[0])
+
+# 3. Get all the elements from numbers except the first two (slice)
+numbers = [3, 1, 4, 1, 5, 9, 1].slice(1-6)
+number_Slice = slice(1-6)
+print(numbers(number_Slice))
+
+
+#4. Check if 9 is an element
+numbers = [3, 1, 4, 1, 5, 9, 1]
+print(numbers[9])
+
